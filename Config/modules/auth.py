7b840d332a0b60bf265dfd1ab2298d8ff2052d4f
SSiva Raja <ishanku@gmail.com>
2022-09-30 06:59:49 -0400

    Update auth.py

--- Config/modules/auth.py
+++ Config/modules/auth.py
@@ -1,11 +1,13 @@
 
 AUTHENTICATION_BACKENDS = [
     "django.contrib.auth.backends.ModelBackend",
-    "allauth.account.auth_backends.AuthenticationBackend",
 ]
-AUTH_USER_MODEL = "users.User"
-LOGIN_REDIRECT_URL = "users:redirect"
-LOGIN_URL = "account_login"
+
+if SOCIALACCOUNT_ENABLED:
+    AUTHENTICATION_BACKENDS += [ "allauth.account.auth_backends.AuthenticationBackend"]
+    AUTH_USER_MODEL = "users.User"
+    LOGIN_REDIRECT_URL = "users:redirect"
+    LOGIN_URL = "account_login"
 
 # PASSWORDS
 PASSWORD_HASHERS = [
@@ -24,9 +26,12 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
-ACCOUNT_ALLOW_REGISTRATION = True#env.bool("DJANGO_ACCOUNT_ALLOW_REGISTRATION", True)
-ACCOUNT_AUTHENTICATION_METHOD = "username"
-ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_EMAIL_VERIFICATION = "mandatory"
-ACCOUNT_ADAPTER = "Roja.users.adapters.AccountAdapter"
-SOCIALACCOUNT_ADAPTER = "Roja.users.adapters.SocialAccountAdapter"
+
+
+if SOCIALACCOUNT_ENABLED:
+    ACCOUNT_ALLOW_REGISTRATION = True  # env.bool("DJANGO_ACCOUNT_ALLOW_REGISTRATION", True)
+    ACCOUNT_AUTHENTICATION_METHOD = "username"
+    ACCOUNT_EMAIL_REQUIRED = True
+    ACCOUNT_EMAIL_VERIFICATION = "mandatory"
+    ACCOUNT_ADAPTER = "Roja.users.adapters.AccountAdapter"
+    SOCIALACCOUNT_ADAPTER = "Roja.users.adapters.SocialAccountAdapter"
