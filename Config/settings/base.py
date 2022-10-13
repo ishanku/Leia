3a1f265afb9c31d36574f723dd99ed235306e2e9
SSiva Raja <ishanku@gmail.com>
2022-10-13 16:58:30 -0400

    Views Update

--- Config/settings/base.py
+++ Config/settings/base.py
@@ -86,7 +86,6 @@ TEMPLATES = [
 
 
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 #
--- Roja/views.py
+++ Roja/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.views.decorators.clickjacking import xframe_options_exempt
 
 # Create your views here.
+@xframe_options_exempt
 def index(request):
     base_url = "{0}://{1}{2}".format(request.scheme, request.get_host(), request.path)
     print("Printing the Base URL")
